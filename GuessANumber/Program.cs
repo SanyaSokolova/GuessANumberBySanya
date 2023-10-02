882217179aeebdf9efa26c96aeff6a57f82a133d
SanyaSokolova <sanya_sokolova@abv.bg>
2023-10-02 16:23:03 +0300

    Guess a number program

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace GuessANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNuber);
+                if (isValid)
+                {
+                    if (playerNuber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNuber > computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else if (playerNuber < computerNumber)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+
+            }
+        }
+    }
+}
